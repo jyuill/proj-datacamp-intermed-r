aa7eee61fc8666af9d9a06984f7ce89b2ff66bc9
jyuill <john@fig4.com>
2016-06-14 20:16:07 -0700

    sapply

--- sapply.R
+++ sapply.R
@@ -27,3 +27,9 @@ unique_letters <- function(name){
 }
 
 sapply(cities,unique_letters)
+
+list(runif(10),runif(10))
+sapply(list(runif(10),runif(10)),
+       function(x) {
+         c(min=min(x),mean=mean(x),max=max(x))
+        })
